6fe46794edd027ee100a863d8b28638a899574ae
Hoplin <45956041+J-hoplin1@users.noreply.github.com>
2020-04-10 17:54:09 +0900

    extractor code

---
+++ YoutubeExtractor.py
@@ -0,0 +1,57 @@
+#-*- coding: utf-8 -*-
+
+#This code and description is written by Hoplin
+#No matter to use it as non-commercial.
+#if there's issue after pip install pytube, install pip install pytube3
+
+import pytube as pt #pip install pytube3
+import re
+import os
+import warnings
+import shutil
+import moviepy.editor as mp
+warnings.filterwarnings("ignore")
+
+#Default save directory
+programa_dir = os.getcwd()
+print("Directory will be generated in : ",programa_dir)
+YTURL = input("Enter Youtube URL : ")
+os.system('cls')
+#Check URL Link Pattern
+YTURLPattern = re.compile("https\:\/\/www\.youtube\.com\/watch[A-Za-z?=.]*")
+ckP = YTURLPattern.match(YTURL)
+
+#if Patter correct
+if ckP:
+    pass
+    YTu = pt.YouTube(YTURL)
+    options = YTu.streams
+
+    for ot in range(len(options)):
+        # value like this : ['<Stream:', 'itag="18"', 'mime_type="video/mp4"', 'res="360p"', 'fps="30fps"', 'vcodec="avc1.42001E"', 'acodec="mp4a.40.2"', 'progressive="True"', 'type="video">']
+        optList = str(options[ot]).split(' ')
+        print(ot+1, '. ' + "MIME Type : " + optList[2].split("\"")[1] + " | " + "RES : " + optList[3].split("\"")[1] + " | " + "VCodec : " + optList[5].split("\"")[1] + " | " + "ACodec : " + optList[6].split("\"")[1] + " | " + "Type : " + optList[-1].split("\"")[1])
+    opNum = int(input("\nEnter option Number : "))
+    os.system('cls')
+    try:
+        print("Downloading files....")
+        defaultfilename = options[opNum - 1].default_filename
+        os.mkdir(programa_dir+"\\" + defaultfilename.split('.mp4')[0] +"\\")
+        regenerateDefaultDir = programa_dir+"\\" + defaultfilename.split('.mp4')[0]
+        options[opNum - 1].download(regenerateDefaultDir)
+        print("Video file(.mp4) download complete....")
+        clip = mp.VideoFileClip(regenerateDefaultDir+"\\"+defaultfilename)
+        clip.audio.write_audiofile(regenerateDefaultDir+"\\"+defaultfilename.split('.mp4')[0] + ".mp3")
+        print("Music file(.mp3) download complete....")
+        print("Download Completed")
+    except IndexError as e:
+        print("Error : Wrong option number.")
+        os.system('cls')
+        time.sleep(1.5)
+    except FileNotFoundError as w:
+        print("Error : Unexpected critical error. Can't re-encode File.")
+        os.system('cls')
+        time.sleep(1.5)
+#if pattern not correct
+else:
+    print("Wrong types of Youtube Link. Please check again.")
